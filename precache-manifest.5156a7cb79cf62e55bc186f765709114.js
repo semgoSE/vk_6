d721268bd4b77b2280efa9e7374d359ae07f72cf
sem1234d <sem1234d@gmail.com>
2020-01-18 22:35:50 +0300

    Updates

---
+++ precache-manifest.5156a7cb79cf62e55bc186f765709114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d30460959b191cb30b8",
+    "url": "/vk_6/static/js/runtime~main.7d304609.js"
+  },
+  {
+    "revision": "0a9fc18049cf24165b58",
+    "url": "/vk_6/static/js/main.0a9fc180.chunk.js"
+  },
+  {
+    "revision": "ef668091077f8123241b",
+    "url": "/vk_6/static/js/2.ef668091.chunk.js"
+  },
+  {
+    "revision": "ef668091077f8123241b",
+    "url": "/vk_6/static/css/2.580d7c02.chunk.css"
+  },
+  {
+    "revision": "6f1c44bb051f3a958dd56730b02c3913",
+    "url": "/vk_6/index.html"
+  }
+];
